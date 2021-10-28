87bde69733f9b10a30da6f0a0a98c7643b1f24de
OlhaMalynovska <olhamalynovska@gmail.com>
2021-10-28 23:06:47 +0300

    5. Write a JavaScript function with conditional statement to sort three numbers.

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-/*4. Write a JavaScript function that accept two integers and 
-display the larger.*/
-function displayMax(a,b) {
-    if (!Number.isInteger(a) || !Number.isInteger(b)) {
-      console.log("Integer parameters expected");
-      return;
+/*5. Write a JavaScript function with conditional statement to sort 
+three numbers.*/
+
+function sort(a,b,c) {
+    if (a>b && a>c) {
+       p1=a;
+       if (b>c) {
+          p2=b;
+          p3=с;
+       } else {
+          p2=с;
+          p3=b;
+       }
+    } else if (b>c && b>a) {
+       p1=b;
+       if (a>c) {
+          p2=a;
+          p3=с;
+       } else {
+          p2=c;
+          p3=a;
+       }
+    } else if (c>a && c>b) {
+       p1=c;
+       if (b>a) {
+          p2=b;
+          p3=a;
+       } else {
+          p2=a;
+          p3=b;
+       } 
     }
-  
-    if (a != b) {
-      console.log("The larger number is " + Math.max(a,b));
-    } else {
-      console.log("Both numbers are equal to " + a);
-    }
-  }
-  
-  displayMax(6, 10);
+ }
+ 
+ sort(5,7,6);
+ 
+ console.log(p3);
+ console.log(p2);
+ console.log(p1)
