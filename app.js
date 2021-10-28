a5f81e3bd05b8bbbd605ed66b10cc033c2acb102
OlhaMalynovska <olhamalynovska@gmail.com>
2021-10-28 23:05:36 +0300

    4. Write a JavaScript function that accept two integers and display the larger

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-/*3.Write a JavaScript function to convert a string in abbreviated 
-form. console.log(abbrev (“Name Surname")) – should be 
-“N.S.” (should convert lower case names to upper)*/
-function abbrev(a) {
-    let res=a.split(" ");
-    let n=res[0][0].toUpperCase();
-    let s=res[1][0].toUpperCase();
-    return `${n}. ${s}.`; 
-}
-
-console.log(abbrev('Olha Malynovska'));
+/*4. Write a JavaScript function that accept two integers and 
+display the larger.*/
+function displayMax(a,b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+      console.log("Integer parameters expected");
+      return;
+    }
+  
+    if (a != b) {
+      console.log("The larger number is " + Math.max(a,b));
+    } else {
+      console.log("Both numbers are equal to " + a);
+    }
+  }
+  
+  displayMax(6, 10);
