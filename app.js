0264655feb9ac0af50d5f94b12cca03a3e8c0bf0
OlhaMalynovska <olhamalynovska@gmail.com>
2021-11-01 22:37:00 +0200

    3. Compute the sum of each individual index value from the given arrays

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-/*
-Write a simple JavaScript program to join all elements of the 
-following array into a string
+/*There are two arrays with individual values, write a 
+JavaScript program to compute the sum of each individual 
+index value from the given arrays and save them to third 
+array
 */
-let array1 = [2,4,5,0];
-console.log(array1.join(" "));
+
+let  array1 = [2,4,5,6,6];
+let  array2 = [4,5,3]; 
+let  array3 = [];
+array3.length = Math.max(array1.length, array2.length);
+for (let i = 0; i < array3.length; i++) {
+    if (i < array1.length) {
+        array3[i]=array1[i];
+    } else {
+        array3[i] = 0;
+    }
+    if (i < array2.length) {
+        array3[i]+=array2[i];
+    }
+    console.log(array3[i]);
+}
+  
