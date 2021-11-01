309e51779e6af4b73ad301642fe5b996ee032b60
OlhaMalynovska <olhamalynovska@gmail.com>
2021-11-01 22:38:30 +0200

    4. Calculate the area and perimeter of a rectangle

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-/*There are two arrays with individual values, write a 
-JavaScript program to compute the sum of each individual 
-index value from the given arrays and save them to third 
-array
-*/
-
-let  array1 = [2,4,5,6,6];
-let  array2 = [4,5,3]; 
-let  array3 = [];
-array3.length = Math.max(array1.length, array2.length);
-for (let i = 0; i < array3.length; i++) {
-    if (i < array1.length) {
-        array3[i]=array1[i];
-    } else {
-        array3[i] = 0;
-    }
-    if (i < array2.length) {
-        array3[i]+=array2[i];
-    }
-    console.log(array3[i]);
+/*Write a JavaScript program to calculate the area and 
+perimeter of a rectangle. Rectangle should be an object with 
+properties width and height and methods getArea() and 
+getPerimeter();*/
+let figure = {
+    width: 10,
+    height: 14,
+    getArea: function() {return this.width * this.height;},
+    getPerimeter: function() {return 2*(this.width + this.height);}
 }
-  
+
+console.log(`The area of rectangle: ${figure.getArea()}`);
+console.log(`The area of rectangle: ${figure.getPerimeter()}`);
