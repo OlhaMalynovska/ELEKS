806d6aa2867d6e42924cd30433235df5bacb29d4
OlhaMalynovska <olhamalynovska@gmail.com>
2021-11-01 22:32:25 +0200

    1. Write a JavaScript function to get the last element of an array

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Write a JavaScript function to get the last element of an 
+array */
+
+function getLast(a) {
+    return a[a.length - 1];
+ }
+ 
+ let array = ["Mo","Tu","Wed","Th","Fr","Sa","Su"];
+ console.log(getLast(array));
+ 
+ 
+ 
