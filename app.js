a6a3446569cb74ec9d95cc32f09731ed9777e0bf
OlhaMalynovska <olhamalynovska@gmail.com>
2021-11-04 23:31:01 +0200

    4. Override method of father’s class

--- app.js
+++ app.js
@@ -1,58 +1,50 @@
-/*Call method of father’s class.*/
-
+/* Override method of father’s class. */
 
 class Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
+    constructor(carModel, carColour, carYear,  maxSpeed) {
         this.model = carModel;
+        this.colour = carColour;
         this.year = carYear;
         this.maxSpeed = maxSpeed; 
     }
 
     displayInfo() {
-        console.log(`Name: ${this.model};
-        Year: ${this.year};
-        Max Speed: ${this.maxSpeed};
-        Type: ${this.type};`);
+        console.log(`The ${this.colour} ${this.type} ${this.model} of ${this.year} with maximal speed ${this.maxSpeed} km/h;`);
     }
 }
 
 class Car extends Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        super(carModel, carYear, maxSpeed);
+    constructor(carModel, carColour, carYear, maxSpeed) {
+        super(carModel, carColour, carYear, maxSpeed);
         this.type = "car";
     }
 
-    transportPeople() {
-        console.log(`I am starting transporting passengers`);
-    }
+    
 }
 
 class Truck extends Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        super(carModel, carYear, maxSpeed);
+    constructor(carModel, carColour, carYear, maxSpeed) {
+        super(carModel, carColour, carYear, maxSpeed);
         this.type = "truck";
     }
 
-    transportContainer() {
-        console.log(`I am starting transporting heavy container`); 
-    }
-
+   
 }
 
-let car1 = new Car('Ford Fiesta', '2005', 190);
+let car1 = new Car('Ford Fiesta', 'red', '2005', '190');
 car1.displayInfo();
 
-let car2 = new Car('Renault Traffic', '2010', 180);
+let car2 = new Car('Renault Traffic', 'yellow', '2010', '180');
 car2.displayInfo();
 
-let car3 = new Car('BMW 005', '2011', 200);
+let car3 = new Car('BMW 005', 'blue', '2011', '200');
 car3.displayInfo();
 
-let truck1 = new Truck('Mann', '2003', 120);
+let truck1 = new Truck('Mann', 'white', '2004', '120');
 truck1.displayInfo();
 
-let truck2 = new Truck('Mercedes', '2008', 140);
+let truck2 = new Truck('Mercedes','black', '2008', '140');
 truck2.displayInfo();
 
-let truck3 = new Truck('Volvo', '2003', 140);
+let truck3 = new Truck('Volvo', 'green', '150');
 truck3.displayInfo();
